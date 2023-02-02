ffdf5c10212afc8f7aaf68d34d9cd9df9c98e0f4
Surafel Workayehu <surafelwork32@gmail.com>
2023-02-02 23:24:52 +0300

    Time: 354 ms (97.10%), Space: 16.1 MB (94.19%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -1,23 +1,36 @@
 class Solution:
+    # def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+    #     c=Counter(words)            
+    #     count=0
+    #     for k in c:
+    #         print(k)
+    #         i = 0
+    #         j = 0
+    #         while(j < len(k) and i < len(s)):
+    #             if(s[i] == k[j]):
+    #                 i = i + 1
+    #                 j = j + 1
+    #             else:
+    #                 i= i + 1
+    #         if(j == len(k)):
+    #             count+=c[k]                
+    #     return count
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        c=Counter(words)
-        # print(c)
-       
-        count=0
-        for k in c:
-            print(k)
-            i = 0
-            j = 0
-            while(j < len(k) and i < len(s)):
-                if(s[i] == k[j]):
-                    i = i + 1
-                    j = j + 1
-                else:
-                    i= i + 1
-            if(j == len(k)):
-                count+=c[k]
-                print(c[k])
-        return count
+    
+        def is_sub(word):
+            index=-1
+            for ch in word:
+                index=s.find(ch,index+1)
+                if index==-1:
+                    return False
+            return True
+
+        c=0
+        for word in words:
+            if is_sub(word):
+                c+=1
+
+        return c
 
 
                 
