2b38bf815aa719770c2b755de26ba70b71550523
Surafel Workayehu <surafelwork32@gmail.com>
2023-02-05 13:36:01 +0300

    Time: 38 ms (58.39%), Space: 13.9 MB (19.81%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if a=='0' and b=='0':
+            return '0'
+        def convert_num(st):       
+            val=0
+            st=st[::-1]
+            for i in range(len(st)):
+                
+                if st[i]=='1':
+                    val+=2**i
+            return val
+        res=""
+        summ=convert_num(a)+convert_num(b)
+        while summ>0:
+            rem=summ%2
+            res+=str(rem)
+            summ=summ//2
+        return res[::-1]
+        
+                
+                
