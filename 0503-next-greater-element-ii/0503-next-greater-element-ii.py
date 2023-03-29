0942b2694cb3813da30f280fda286d99be8c1358
Surafel Workayehu <surafelwork32@gmail.com>
2023-03-29 14:38:28 +0300

    Time: 224 ms (53.71%), Space: 15.5 MB (94.93%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -6,18 +6,12 @@ class Solution:
 
         for i in range(2*n-1,-1,-1):
             num=nums[i%n]
-            index=i+1
-
             while stack and stack[-1]<=num:
                 stack.pop(-1)
-
             if i<n:
                 if len(stack)==0:
                     nums[i]=-1
-
                 else:
                     nums[i]=stack[-1]
-
             stack.append(num)
-
         return nums
