42f5f2c1481432240c87d7be4c5189a903fc978d
Surafel Workayehu <surafelwork32@gmail.com>
2023-04-07 20:56:04 +0300

    Time: 772 ms (24.37%), Space: 15.3 MB (75.13%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,21 +6,38 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        counter = defaultdict(int)
-        counter[0]=1
-        # print(counter)
-        self.res = 0
-        def helper(root, cursum):
+        count=0
+        def helper(root,target):
+            nonlocal count
             if not root:
-                return
-            cursum += root.val
-            self.res += counter[cursum-targetSum]
-            counter[cursum] += 1
-            helper(root.left, cursum)
-            helper(root.right, cursum)
-            counter[cursum] -= 1
-        helper(root, 0)
-        return self.res
+                return 
+            if target==root.val:
+                count+=1
+            helper(root.left,target-root.val)
+            helper(root.right,target-root.val)
+        def dfs(root,target):
+            if not root:
+                return 
+            helper(root,target)
+            dfs(root.left,target)
+            dfs(root.right,target)
+        dfs(root,targetSum)
+        return count
+#         counter = defaultdict(int)
+#         counter[0]=1
+#         # print(counter)
+#         self.res = 0
+#         def helper(root, cursum):
+#             if not root:
+#                 return
+#             cursum += root.val
+#             self.res += counter[cursum-targetSum]
+#             counter[cursum] += 1
+#             helper(root.left, cursum)
+#             helper(root.right, cursum)
+#             counter[cursum] -= 1
+#         helper(root, 0)
+#         return self.res
     
     
                 
