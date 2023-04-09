6d4787c58ac19ed234840191d9c69b7357e82512
Surafel Workayehu <surafelwork32@gmail.com>
2023-04-09 16:37:11 +0300

    Time: 85 ms (98.00%), Space: 13.9 MB (70.39%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,26 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for row in board:
-            seen = set()
+            visited=set()
             for col in row:
-                if col != "." and col in seen:
+                if col!='.' and col in visited:
                     return False
-                seen.add(col)
-                
-        for col in range(len(board)):
-            seen = set()
+                visited.add(col)
+        n=len(board)
+        for col in range(n):
+            visited=set()
             for row in board:
-                if row[col] != "." and row[col] in seen:
+                if row[col]!='.' and row[col] in visited:
                     return False
-                seen.add(row[col])
-                
-        for row3 in range(0, 9, 3):
-            for col3 in range(0, 9, 3):
-                seen = set()
-                for row in range(row3, row3+3):
-                    for col in range(col3, col3+3):
-                        if board[row][col] != "." and board[row][col] in seen:
+                visited.add(row[col])
+        for row3 in range(0,9,3):
+            for col3 in range(0,9,3):
+                visited=set()
+                for row in range(row3,row3+3):
+                    for col in range(col3,col3+3):
+                        if board[row][col]!='.' and board[row][col] in visited:
                             return False
-                        seen.add(board[row][col])
-                    
+                        visited.add(board[row][col])
         return True
+    
+    
