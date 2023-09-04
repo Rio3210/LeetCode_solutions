26906677a453bdbc2735d7d95befec274baef1e6
Surafel Workayehu <surafelwork32@gmail.com>
2023-09-04 11:58:21 +0300

    Time: 548 ms (19.16%), Space: 128.1 MB (13.91%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,17 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        # return True
+        cache={}
+        def helper(l,r):
+            if l>r:
+                return 0
+            if (l,r) in cache:
+                return cache[(l,r)]
+            evn= True if r %2 ==0 else 0
+            
+            left= piles[l] if evn else 0
+            right = piles[r] if evn else 0 
+            
+            cache[(l,r)]=max( helper(l+1,r)+ left, helper(l,r-1)+right)
+            return cache[(l,r)]
+        return helper(0,len(piles)-1) > len(piles)//2
