6cbe8f42a2052af593c9ee576272f526f0ae1cfb
Surafel Workayehu <surafelwork32@gmail.com>
2023-10-17 22:22:41 +0300

    Time: 171 ms (23.99%), Space: 17.8 MB (16.20%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,16 +1,63 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.string = None
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+        
+    def charToIndex(self,char):
+        return ord(char) - ord("a")
+
+    def insert(self, word: str) -> None:
+
+        tempRoot = self.root
+
+        for char in word:
+
+            indx = self.charToIndex(char)
+
+            if not tempRoot.children[indx]:
+                tempRoot.children[indx] = TrieNode()
+
+            tempRoot = tempRoot.children[indx]
+
+
+        tempRoot.string = word
+
+
 class Solution:
+
+    def __init__(self):
+        self.ans = ""
+        
+
+    def dfs(self, root):
+        for child in root.children:
+            if child and child.string:
+                if len(child.string) > len(self.ans):
+                    self.ans = child.string
+
+                self.dfs(child)
+    
+    
     def longestWord(self, words: List[str]) -> str:
-        
-        words.sort()  
-        valid_words=set()
-        longest_word=""
-        
+
+        trie = Trie()
+       
         for word in words:
-            pref=word[:-1]
-            if pref=="" or pref in valid_words:
-                valid_words.add(word)
-                if len(word)>len(longest_word):
-                    longest_word=word
-        return longest_word
+            trie.insert(word)
+
         
+        self.dfs(trie.root)
+        
+        return self.ans
+
+        
+
+
         
