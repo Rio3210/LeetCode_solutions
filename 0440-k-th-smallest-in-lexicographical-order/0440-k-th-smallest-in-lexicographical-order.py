5559b476c349a5b53d15e027de9816419046da31
Surafel Workayehu <surafelwork32@gmail.com>
2023-10-21 16:33:31 +0300

    Time: 34 ms (87.57%), Space: 16.3 MB (51.35%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def fn(x): 
+            ans, diff = 0, 1
+            while x <= n: 
+                ans += min(n - x + 1, diff)
+                x *= 10 
+                diff *= 10 
+            return ans 
+        
+        x = 1
+        while k > 1: 
+            cnt = fn(x)
+            if k > cnt: k -= cnt; x += 1
+            else: k -= 1; x *= 10 
+        return x
